94ccfe368559bc8f3715eee95b61801077dcdbd6
Jocelyn Grelier <grelierjocelyn@gmail.com>
2025-01-13 16:39:20 +0100

    user & Guest mode + controller + migration :v:

--- API/database/migrations/2025_01_13_143236_create_guests_table.php
+++ API/database/migrations/2025_01_13_143236_create_guests_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('guests', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id');
+            $table->foreignIdFor(\App\Models\User::class);
             $table->string('first_name');
             $table->string('last_name');
             $table->string('email')->nullable();
