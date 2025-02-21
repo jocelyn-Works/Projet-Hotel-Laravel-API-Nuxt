1c731982a5f49bc86314bdd8d547680a08e470c6
Eric Oriez <ericoriez@live.fr>
2025-02-21 16:52:33 +0100

    chambre type

--- FRONT/pages/index.vue
+++ FRONT/pages/index.vue
@@ -3,9 +3,11 @@
   <HomeAppHeader />
   <Frise src="/ImagesHomepage/frise.svg" />
   <HomePresentation />
+  <RoomTypes />
 </template>
 
 <script setup>
   import Frise from "~/components/Frise.vue";
   import HomePresentation from "~/components/Home/HomePresentation.vue";
+  import RoomTypes from "~/components/Home/RoomTypes.vue";
 </script>
