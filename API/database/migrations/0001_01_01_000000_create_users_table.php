4240cf32610158c38f909d8f3f101ec1ddb9544d
jocelyn <grelierjocelyn@gmail.com>
2025-02-26 15:12:55 +0100

    user model & migration

--- API/database/migrations/0001_01_01_000000_create_users_table.php
+++ API/database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
             $table->uuid('id')->primary();
             $table->string('first_name');
             $table->string('last_name');
+            $table->string('phone');
+            $table->string('adress');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
