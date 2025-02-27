193670f2bceef92483182137785181d95e087519
Zakuross <zakariya.belmokhtar@le-campus-numerique.fr>
2025-02-27 15:06:29 +0100

    migration User

--- API/database/migrations/0001_01_01_000000_create_users_table.php
+++ API/database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,8 +15,6 @@ return new class extends Migration
             $table->id('id')->primary();
             $table->string('first_name');
             $table->string('last_name');
-            $table->string('phone');
-            $table->string('adress');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
