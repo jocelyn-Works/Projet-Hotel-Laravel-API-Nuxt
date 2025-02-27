d16f424da4d6b749be45cb12fdac3c4fa19b805d
Eric Oriez <ericoriez@live.fr>
2025-02-27 10:22:11 +0100

    color white button

--- FRONT/components/Button/AppMenu.vue
+++ FRONT/components/Button/AppMenu.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="fixed top-0 left-0 ml-6 mt-8 z-10 rounded bg-dark-blue-500 bg-opacity-80">
     <div class="flex items-center justify-center w-14 h-14 p-2" @click="openNavigation">
-      <UIcon name="proicons:text-description" class="w-full h-full cursor-pointer" />
+      <UIcon name="proicons:text-description" class="w-full h-full cursor-pointer bg-white" />
     </div>
   </nav>
 </template>
