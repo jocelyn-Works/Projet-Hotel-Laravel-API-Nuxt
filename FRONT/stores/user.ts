3b4036dc56f16ad33263d582d2a32c4f1d19cb9d
jocelyn <grelierjocelyn@gmail.com>
2025-03-19 12:18:42 +0100

    add user.ts pinia -> user info

---
+++ FRONT/stores/user.ts
@@ -0,0 +1,37 @@
+import { defineStore } from 'pinia';
+
+const apiUrl = import.meta.env.VITE_API_URL;
+
+interface UserState {
+  user: any | null;
+  token: string | null;
+}
+
+export const useUserStore = defineStore('user', {
+  state: (): UserState => ({
+    user: null,
+    token: localStorage.getItem('authToken') || null,
+  }),
+  actions: {
+    async fetchUser() {
+      if (!this.token) return;
+
+      const { data, error } = await useFetch(`${apiUrl}/user`, {
+        headers: {
+          'Authorization': `Bearer ${this.token}`,
+        },
+      });
+
+      if (data.value) {
+        this.user = data.value;
+      } else if (error.value) {
+        console.error('Erreur lors de la récupération de l\'utilisateur :', error.value);
+      }
+    },
+    logout() {
+      this.user = null;
+      this.token = null;
+      localStorage.removeItem('authToken');
+    },
+  },
+});
