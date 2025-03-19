1dba04fbf9e2a926a3260f88c9f35eb581ee4517
jocelyn <grelierjocelyn@gmail.com>
2025-03-19 12:20:09 +0100

    update navigation with user conected

--- FRONT/components/Navigation/Navigation.vue
+++ FRONT/components/Navigation/Navigation.vue
@@ -1,38 +1,60 @@
 <template>
-
-    <nav class="fixed top-0 left-0 w-full md:w-2/5 bg-belgian-500 border border-dark-blue-500 rounded-br-lg z-10">
-      <div class="flex justify-between items-center bg-dark-blue-500 p-4">
-        <ButtonLogin  />
-        <UIcon name="heroicons-solid:x-mark" class="w-6 h-6 text-white cursor-pointer" @click="closeNavigation" />
+  <nav class="fixed top-0 left-0 w-full md:w-2/5 bg-belgian-500 border border-dark-blue-500 rounded-br-lg z-10">
+    <div class="flex justify-between items-center bg-dark-blue-500 p-4">
+      <div v-if="user">
+        <ButtonLogout />
       </div>
-      <div class="p-6 satisfy text-dark-blue-500">
-        <ul class="text-2xl p-5 m-3">
-            <li>
-              <NuxtLink to="/" class="hover:text-gold-300">Accueil</NuxtLink>
-            </li>
-            <li>
-              <NuxtLink to="/booking" class="hover:text-gold-300">Booking</NuxtLink></li>
-            <li>Mes reservations</li>
-            <li>Chambres et suites</li>
-            <li>Bars et Restaurants</li>
-          <li>
-            <NuxtLink to="/admin" class="hover:text-gold-300">Dashbord</NuxtLink>
-          </li>
-        </ul>
+      <div v-else>
+        <ButtonLogin />
       </div>
-      <AppFooter class="rounded-br-lg" />
-    </nav>
+      <div v-if="user">
+        <p>Bienvenue, {{ user.first_name }} {{ user.last_name}} !</p>
+      </div>
+      <UIcon name="heroicons-solid:x-mark" class="w-6 h-6 text-white cursor-pointer" @click="closeNavigation" />
+    </div>
+    <div class="p-6 satisfy text-dark-blue-500">
+      <ul class="text-2xl p-5 m-3">
+        <li>
+          <NuxtLink to="/" class="hover:text-gold-300">Accueil</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/booking" class="hover:text-gold-300">Booking</NuxtLink>
+        </li>
+        <li>Mes réservations</li>
+        <li>Chambres et suites</li>
+        <li>Bars et Restaurants</li>
+        <li v-if="user">
+          <NuxtLink to="/admin" class="hover:text-gold-300">Dashboard</NuxtLink>
+        </li>
+      </ul>
+    </div>
+    <AppFooter class="rounded-br-lg" />
+  </nav>
+</template>
 
-  </template>
-  
-  <script setup>
+<script setup lang="ts">
 import { ButtonLogin } from '#components';
 import { useUiStore } from '~/stores/ui';
+import { useUserStore } from '~/stores/user';
+import { onMounted, computed } from 'vue';
 
 const uiStore = useUiStore();
+const userStore = useUserStore();
+
+onMounted(async () => {
+  await userStore.fetchUser();
+});
+
+const user = computed(() => userStore.user);
 
 const closeNavigation = () => {
   uiStore.closeComponent();
+};
+
+const logout = () => {
+  userStore.logout();
+  // Rediriger l'utilisateur vers la page de connexion ou d'accueil après la déconnexion
+  // navigateTo('/login');
 };
 </script>
 
@@ -53,6 +75,3 @@ const closeNavigation = () => {
   transform: translateX(0);
 }
 </style>
-
-
-  
