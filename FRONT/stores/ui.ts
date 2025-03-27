22b2cfb3fc856cb7f761c07e51e09113be719105
jocelyn <grelierjocelyn@gmail.com>
2025-03-27 10:18:25 +0100

    add option components

--- FRONT/stores/ui.ts
+++ FRONT/stores/ui.ts
@@ -1,7 +1,7 @@
 import { defineStore } from 'pinia';
 
 // Définir un type pour les composants possibles
-type ComponentName = 'navigation' | 'login'  | 'signup' | 'dates'| 'cart' | null;
+type ComponentName = 'navigation' | 'login'  | 'signup' | 'dates'| 'cart' |'option'| null;
 
 export const useUiStore = defineStore('ui', {
   state: () => ({
